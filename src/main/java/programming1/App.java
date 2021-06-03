1ccd05f042ee4a94c50ffdaff17e482d1f771296
dilly <dillywoodjean@gmail.com>
2021-06-03 19:34:05 -0400

    *

--- src/main/java/programming1/App.java
+++ src/main/java/programming1/App.java
@@ -1,4 +1,37 @@
+/*
+Quotation marks are often used to denote the start and end of a string. But sometimes we need to print out the quotation marks themselves by using escape characters.
+
+Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output.
+
+Example Output
+What is the quote? These aren't the droids you're looking for.
+Who said it? Obi-Wan Kenobi
+Obi-Wan Kenobi says, "These aren't the droids you're looking for."
+
+Constraints
+Use a single output statement to produce this output, using appropriate string-escaping techniques for quotes.
+If your language supports string interpolation or string substitution, don’t use it for this exercise. Use string concatenation instead.
+ */
+
 package programming1;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        System.out.print("What is the quote? ");
+        String quote=in.nextLine();
+
+        System.out.print("Who said it? ");
+        String name=in.nextLine();
+
+        String output= name + " says, "+ "\""+quote+"\"";
+
+        System.out.println(output);
+
+
+
+    }
 }
